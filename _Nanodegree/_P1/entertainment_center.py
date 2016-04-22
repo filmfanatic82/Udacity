ef64ef0e29acfb9e21af77ba10dc162699d0deac
jesshd <jesshd@jesshd-macbookair.roam.corp.google.com>
2016-04-22 17:19:10 -0400

    updates to movie site files

--- _Nanodegree/_P1/entertainment_center.py
+++ _Nanodegree/_P1/entertainment_center.py
@@ -1,5 +1,20 @@
 import media
+import fresh_tomatoes
 
 toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life", "https://amandawinter.files.wordpress.com/2010/10/600full-toy-story-poster.jpg", "https://www.youtube.com/watch?v=4KPTXpQehio")
 
-print(toy_story.storyline)
+#print(toy_story.storyline)
+#toystory.show_trailer()
+
+avatar = media.Movie("Avatar", "A marine on an alien planet", "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg", "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+school_of_rock = media.Movie("School of Rock", "Using Rock music to learn", "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg", "https://www.youtube.com/watch?v=3PsUJFEBC74")
+
+ratatouille = media.Movie("Ratatouille", "A rat is a chef in Paris", "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg", "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+
+midnight_in_paris = media.Movie("Midnight in Paris", "Go back in time to meet authors", "https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg","https://www.youtube.com/watch?v=DMXX_udM5lM")
+
+hunger_games = media.Movie("Hunger Games", "A really real reality show", "https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg", "https://www.youtube.com/watch?v=mfmrPu43DF8")
+
+movies = [toy_story, avatar, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
+fresh_tomatoes.open_movies_page(movies)
